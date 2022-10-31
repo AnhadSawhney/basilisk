044d325fdc4c738ffa5c15927033457761342533
Anhad Sawhney <sawhney.anhad@gmail.com>
2022-10-31 19:55:06 -0400

    get firmware setup

---
+++ Basilisk_Firmware/src/main.cpp
@@ -0,0 +1,121 @@
+#include <Arduino.h>
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+#include <Adafruit_MLX90640.h>
+
+Adafruit_MLX90640 mlx;
+float frame[32*24]; // buffer for full frame of temperatures
+
+#define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 1      // Total number of panels chained one to another
+
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
+// uncomment *one* of the below
+//#define PRINT_TEMPERATURES
+#define PRINT_ASCIIART
+#define LED 2
+
+const char ascii_LUT[] = " .:-=+*#%@";
+
+void setup() {
+    // Module configuration
+    HUB75_I2S_CFG mxconfig(
+      PANEL_RES_X,   // module width
+      PANEL_RES_Y,   // module height
+      PANEL_CHAIN    // Chain length
+    );
+
+    // Display Setup
+    dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+    dma_display->begin();
+    dma_display->setBrightness8(90); //0-255
+    dma_display->clearScreen();
+    dma_display->fillScreen(0xFF);
+
+    pinMode(LED, OUTPUT);
+
+    MLX_Setup();
+}
+
+void loop() {
+  digitalWrite(LED, HIGH); 
+  delay(500);             
+  digitalWrite(LED, LOW);  
+  delay(500);          
+  print_IR_frame();
+}
+
+void MLX_Setup() {
+  while (!Serial) delay(10);
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("Adafruit MLX90640 Simple Test");
+  if (! mlx.begin(MLX90640_I2CADDR_DEFAULT, &Wire)) {
+    Serial.println("MLX90640 not found!");
+    while (1) delay(10);
+  }
+  Serial.println("Found Adafruit MLX90640");
+
+  Serial.print("Serial number: ");
+  Serial.print(mlx.serialNumber[0], HEX);
+  Serial.print(mlx.serialNumber[1], HEX);
+  Serial.println(mlx.serialNumber[2], HEX);
+  
+  //mlx.setMode(MLX90640_INTERLEAVED);
+  mlx.setMode(MLX90640_CHESS);
+  Serial.print("Current mode: ");
+  if (mlx.getMode() == MLX90640_CHESS) {
+    Serial.println("Chess");
+  } else {
+    Serial.println("Interleave");    
+  }
+
+  mlx.setResolution(MLX90640_ADC_18BIT);
+  Serial.print("Current resolution: ");
+  mlx90640_resolution_t res = mlx.getResolution();
+  switch (res) {
+    case MLX90640_ADC_16BIT: Serial.println("16 bit"); break;
+    case MLX90640_ADC_17BIT: Serial.println("17 bit"); break;
+    case MLX90640_ADC_18BIT: Serial.println("18 bit"); break;
+    case MLX90640_ADC_19BIT: Serial.println("19 bit"); break;
+  }
+
+  mlx.setRefreshRate(MLX90640_2_HZ);
+  Serial.print("Current frame rate: ");
+  mlx90640_refreshrate_t rate = mlx.getRefreshRate();
+  switch (rate) {
+    case MLX90640_0_5_HZ: Serial.println("0.5 Hz"); break;
+    case MLX90640_1_HZ: Serial.println("1 Hz"); break; 
+    case MLX90640_2_HZ: Serial.println("2 Hz"); break;
+    case MLX90640_4_HZ: Serial.println("4 Hz"); break;
+    case MLX90640_8_HZ: Serial.println("8 Hz"); break;
+    case MLX90640_16_HZ: Serial.println("16 Hz"); break;
+    case MLX90640_32_HZ: Serial.println("32 Hz"); break;
+    case MLX90640_64_HZ: Serial.println("64 Hz"); break;
+  }
+}
+
+void print_IR_frame() {
+  if (mlx.getFrame(frame) != 0) {
+    Serial.println("Failed");
+    return;
+  }
+  Serial.println();
+  Serial.println();
+  for (uint8_t h=0; h<24; h++) {
+    for (uint8_t w=0; w<32; w++) {
+      float t = frame[h*32 + w];
+#ifdef PRINT_TEMPERATURES
+      Serial.print(t, 1);
+      Serial.print(", ");
+#endif
+#ifdef PRINT_ASCIIART
+      uint8_t idx = (uint8_t)map(t, 20, 40, 0, 10);
+      Serial.print(ascii_LUT[idx]);
+#endif
+    }
+    Serial.println();
+  }
+}

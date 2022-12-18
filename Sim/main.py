a22dfcc684cb2e4fabdfeb3eb7d85914a468dc25
kjander <kjander@mit.edu>
2022-12-17 19:41:50 -0500

    blinking skin

--- Sim/main.py
+++ Sim/main.py
@@ -1,13 +1,13 @@
 import pygame
 import sys
 
-#hello anhad
 # create a 64x64 pygame window
 # record the mouse cursor position and display it onscrewen
 
+screen = pygame.display.set_mode((64,64))
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((64,64))
     pygame.display.set_caption('The All Seeing Eye')
     clock = pygame.time.Clock()
     while True:
@@ -15,7 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        screen.fill((0,0,0))
+            if event.type == pygame.MOUSEBUTTONUP:
+                blink()
+        #screen.fill((0,0,0))
         mouse_pos = pygame.mouse.get_pos()
         print(mouse_pos)
         pygame.display.update()
@@ -30,10 +32,33 @@ def load_eyeball_images():
     return eyeball
 
 def load_skin_images():
-    skin = []
-    for i in range(1,14):
-        skin.append(pygame.image.load('skin/skin'+str(i)+'.png'))
+    skin = [pygame.image.load('skin/blinking skin only1.png')]
+    for i in range(13,21):
+        skin.append(pygame.image.load('skin/blinking skin only'+str(i)+'.png'))
     return skin
+
+def blink():
+    #close eye
+    skins = load_skin_images()
+    print(len(skins))
+    for i in range(0,9):
+        #print(i)
+        screen.fill((0,0,0))
+        screen.blit(skins[i], (0,0))
+        pygame.display.update()
+        pygame.time.delay(150)
+    #open eye
+    for i in range(7, -1, -1):
+        #print(i)
+        screen.fill((0,0,0))
+        screen.blit(skins[i], (0,0))
+        pygame.display.update()
+        pygame.time.delay(150)
+    screen.fill((0,0,0))
+
+def look_at_mouse():
+    mouse_pos = pygame.mouse.get_pos()
+    #TODO
 
 if __name__ == '__main__':
     main()

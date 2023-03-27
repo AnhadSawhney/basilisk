ac7f9302c624b46bb6b39b975803601d359c91fb
Anhad Sawhney <sawhney.anhad@gmail.com>
2023-03-26 23:25:43 -0600

    warp the eye

--- Sim/main.py
+++ Sim/main.py
@@ -13,12 +13,152 @@ from PIL import Image
 # 5. Draw the iris at the exact correct position
 # 6. Blend between the eyeball backgrounds to get a smooth transition between positions
 
-screen = pygame.display.set_mode((64,64))
+#screen = pygame.display.set_mode((256,256))
+# create a screen that can be resized, starts at 256x256
+screen = pygame.display.set_mode((256,256), pygame.RESIZABLE)
+pixel_scale = screen.get_width() / 64
+array_2d = []
+blink = 0
+
+#takes a loaded png image and turns it into a 2d pixel array
+def array_from_image(path):
+    #turn image into pixel array
+    #64x64 image -> one array with 64 arrays with 64 elements each (r,c)
+    #(R,G,B,A)
+    im = Image.open(path)
+    rgba_im = im.convert("RGBA")
+    pix = rgba_im.load()
+    image_array = list(rgba_im.getdata())
+    array_2d = [[rgba_im.getpixel((i,j)) for j in range(64)] for i in range(64)]
+    return array_2d
+
+skins = []
+for i in range(1,10):
+    skins.append(array_from_image('skin/skin'+str(i)+'.png'))
+
+def composite_array(over, under, alpha):
+    for i in range(0,64):
+        for j in range(0,64):
+            if over[i][j][3] > 0:
+                under[i][j] = over[i][j]
+            #alpha = over[i][j][3]
+            # TODO: element wise multiplication
+            #under[i][j] = under[i][j] * (255-alpha) + over[i][j] * alpha
+
+#pupil data
+# offset, width
+pupil_data = [
+    [-2, 2],
+    [-3, 4],
+    [-3, 5],
+    [-4, 7],
+    [-4, 7],
+    [-5, 8],
+    [-5, 9],
+    [-5, 9],
+    [-5, 10],
+    [-5, 10],
+    [-5, 10],
+    [-5, 10],
+    [-5, 10],
+    [-5, 11],
+    [-5, 11],
+    [-5, 11],
+    [-6, 12],
+    [-6, 12],
+    [-6, 12],
+    [-6, 12],
+    [-6, 12],
+    [-6, 12],
+    [-6, 12],
+    [-6, 13],
+    [-6, 13],
+    [-5, 12],
+    [-5, 12],
+    [-5, 12],
+    [-5, 12],
+    [-5, 12],
+    [-5, 12],
+    [-5, 12],
+    [-5, 11],
+    [-5, 11],
+    [-5, 11],
+    [-4, 10],
+    [-4, 10],
+    [-4, 10],
+    [-4, 10],
+    [-4, 10],
+    [-3, 9],
+    [-3, 9],
+    [-2, 8],
+    [-2, 8],
+    [-2, 7],
+    [-2, 7],
+    [-1, 5],
+    [0, 4],
+    [1,2]
+]
+
+pupil_height = len(pupil_data)
+
+def render_pupil(pos):
+    global array_2d
+    #print(pos)
+    #print(pupil_data)
+    #print(pupil_height)
+    for i in range(pupil_height):
+        y = int(pos[1] - pupil_height/2 + i)
+        if y < 0 or y > 63:
+            continue
+        offset = pupil_data[i][0]
+
+        # move the offset if the pupil is close to the edge
+        x = int(pos[0])
+        alpha = (x - 32)/1.5
+        offset += alpha*(1-(1-y/32)**2)
+
+        width = pupil_data[i][1]
+        for j in range(width):
+            x = int(pos[0] + j + offset)
+            if x < 0 or x > 63:
+                continue
+            array_2d[x][y] = [0,0,0,255]
+
+def update_array():
+    global blink
+    global array_2d
+    array_2d = [[[255,255,255,255] for j in range(64)] for i in range(64)]
+    pos = pygame.mouse.get_pos()
+    pos = (int(pos[0]/pixel_scale), int(pos[1]/pixel_scale))
+    render_pupil(pos)
+
+    if blink == -1:
+        blink = 1
+    if blink >= len(skins):
+        composite_array(skins[2*len(skins)-blink-1], array_2d, 255)
+        #print(2*len(skins)-blink-1)
+    else:
+        composite_array(skins[blink], array_2d, 255)
+        #print(blink)
+    if blink > 0:
+        blink += 1
+        if blink >= len(skins)*2:
+            blink = 0
+    
+
+def draw_from_array():
+    global array_2d
+    global pixel_scale
+    # draw the 64x64 array on the screen, scaling it to the size of the screen
+    for i in range(0,64):
+        for j in range(0,64):
+            pygame.draw.rect(screen, array_2d[i][j], (i*pixel_scale,j*pixel_scale,pixel_scale,pixel_scale))
 
 def main():
     pygame.init()
     pygame.display.set_caption('The All Seeing Eye')
     clock = pygame.time.Clock()
+    global screen, pixel_scale, blink
     #iris_only = pygame.image.load('eyeball/iris_only.png')
     while True:
         for event in pygame.event.get():
@@ -26,88 +166,20 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONUP:
-                #blink()
-                get_2d_array()
-                draw_from_array()
+                if blink == 0:
+                    blink = -1 # trigger blink
+            # handle screen resize
+            if event.type == pygame.VIDEORESIZE:
+                size = min(event.w, event.h)
+                size = size - (size % 64)
+                screen = pygame.display.set_mode((size, size), pygame.RESIZABLE)
+                pixel_scale = size / 64
         #screen.fill((0,0,0))
-        #mouse_pos = pygame.mouse.get_pos()
-        #print(mouse_pos)
         #move_pupil()
-        #get_2d_array()
+        update_array()
+        draw_from_array()
         pygame.display.update()
-        clock.tick(60)
-
-# load all the images in the eyeball folder and save them in a list
-# load all the images in the skin folder and save them in a list
-def load_eyeball_images():
-    eyeball = []
-    for i in range(1,14):
-        eyeball.append(pygame.image.load('eyeball/eyeball'+str(i)+'.png'))
-    return eyeball
-
-def load_skin_images():
-    skin = [pygame.image.load('skin/blinking skin only1.png')]
-    for i in range(13,21):
-        skin.append(pygame.image.load('skin/blinking skin only'+str(i)+'.png'))
-    return skin
-
-def blink():
-    #close eye
-    skins = load_skin_images()
-    iris_only = pygame.image.load('eyeball/iris_only.png')
-    pupil = pygame.image.load('eyeball/pupil.png')
-    for i in range(0,9):
-        #print(i)
-        screen.fill((0,0,0))
-        screen.blit(iris_only, (0,0))
-        screen.blit(pupil, (0,0))
-        screen.blit(skins[i], (0,0))
-        pygame.display.update()
-        pygame.time.delay(150)
-    #open eye
-    for i in range(7, -1, -1):
-        #print(i)
-        screen.fill((0,0,0))
-        screen.blit(iris_only, (0,0))
-        screen.blit(pupil, (0,0))
-        screen.blit(skins[i], (0,0))
-        pygame.display.update()
-        pygame.time.delay(150)
-    screen.fill((0,0,0))
-
-def look_at_mouse():
-    mouse_pos = pygame.mouse.get_pos()
-    #TODO
-
-def move_pupil():
-    #first just placing the pupil
-    iris_only = pygame.image.load('eyeball/iris_only.png')
-    pupil = pygame.image.load('eyeball/pupil.png')
-    skin = pygame.image.load('skin/blinking skin only1.png')
-    mouse_pos = pygame.mouse.get_pos()
-    screen.blit(iris_only, (0,0))
-    screen.blit(pupil, (mouse_pos[0] - 32, mouse_pos[1] - 32))
-    screen.blit(skin, (0,0))
-
-#takes a loaded png image and turns it into a 2d pixel array
-def get_2d_array():
-    #turn image into pixel array
-    #64x64 image -> one array with 64 arrays with 64 elements each (r,c)
-    #(R,G,B,A)
-    im = Image.open('eyeball/iris_only.png')
-    rgba_im = im.convert("RGBA")
-    pix = rgba_im.load()
-    image_array = list(rgba_im.getdata())
-    array_2d = [[rgba_im.getpixel((i,j)) for j in range(64)] for i in range(64)]
-    return array_2d
-
-def draw_from_array():
-    array_2d = get_2d_array()
-    for i in range(64):
-        for j in range(64):
-            color = array_2d[i][j]
-            screen.set_at((i, j), color)
-            #print("setting")
+        clock.tick(33)
 
 if __name__ == '__main__':
     main()
